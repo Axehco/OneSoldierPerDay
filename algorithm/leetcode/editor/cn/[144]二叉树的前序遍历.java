49232685241a899cabe32f9de798d374a0fd5592
Axehco <mbozhong@outlook.com>
2021-11-22 22:02:37 +0800

    init onesoldierperday

---
+++ algorithm/leetcode/editor/cn/[144]二叉树的前序遍历.java
@@ -0,0 +1,49 @@
+package leetcode.editor.cn;
+import datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class preorderTraversalSolution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preOrder(root, res);
+        return res;
+    }
+
+    public static void preOrder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        preOrder(root.left, res);
+        preOrder(root.right, res);
+    }
+
+    public static void main(String[] args) {
+        preorderTraversalSolution solution = new preorderTraversalSolution();
+        TreeNode root = TreeNode.deserialize("1,null,2,3,null");
+//        TreeNode root = TreeNode.deserialize("");
+//        TreeNode root = TreeNode.deserialize("1,null,null");
+//        TreeNode root = TreeNode.deserialize("1,2,null");
+//        TreeNode root = TreeNode.deserialize("1,null,2");
+        System.out.println(solution.preorderTraversal(root));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
