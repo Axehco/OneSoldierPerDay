d05ca642f1241bafccfd51b29b27b42da394f73d
Axehco <mbozhong@outlook.com>
2021-11-22 22:36:28 +0800

    144. 二叉树的前序遍历 增加迭代遍历

--- algorithm/leetcode/editor/cn/[144]二叉树的前序遍历.java
+++ algorithm/leetcode/editor/cn/[144]二叉树的前序遍历.java
@@ -1,8 +1,10 @@
 package leetcode.editor.cn;
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import datastructure.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
@@ -21,6 +23,11 @@ import java.util.List;
  * }
  */
 class preorderTraversalSolution {
+    /**
+     * 递归写法
+     * @param root
+     * @return
+     */
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         preOrder(root, res);
@@ -36,6 +43,32 @@ class preorderTraversalSolution {
         preOrder(root.right, res);
     }
 
+    /**
+     * 迭代写法，借助栈来完成：入栈顺序：根->右子树->左子树
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversalIteration(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode treeNode = stack.peek();
+            res.add(treeNode.val);
+            stack.pop();
+            if (treeNode.right != null) {
+                stack.push(treeNode.right);
+            }
+            if (treeNode.left != null) {
+                stack.push(treeNode.left);
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         preorderTraversalSolution solution = new preorderTraversalSolution();
         TreeNode root = TreeNode.deserialize("1,null,2,3,null");
@@ -45,6 +78,9 @@ class preorderTraversalSolution {
         TreeNode root = TreeNode.deserialize("1,null,null");
         TreeNode root = TreeNode.deserialize("1,2,null");
         TreeNode root = TreeNode.deserialize("1,null,2");*/
+
+        // 迭代写法调试
+        System.out.println(solution.preorderTraversalIteration(root));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
