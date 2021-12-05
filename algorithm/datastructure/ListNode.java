eba30c2f0e9578826858dc5606ff93c299bf9d67
Axehco <mbozhong@outlook.com>
2021-12-05 15:30:43 +0800

    链表构造工具类

---
+++ algorithm/datastructure/ListNode.java
@@ -0,0 +1,89 @@
+package datastructure;
+
+/**
+ * @author Axehco
+ * @createTime 2021年12月05日 15:06
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    /**
+     * 尾节点构造器
+     * @param val 节点值
+     */
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    /**
+     * 链表节点构造器
+     * @param val 节点值
+     * @param next 指向的下一个节点
+     */
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    /**
+     * 无参构造器
+     */
+    public ListNode() {
+
+    }
+
+    /**
+     * 构造链表
+     * @param nums 构造链表的数组
+     * @param withHead 是否需要头节点
+     * @return 构造完成的链表
+     */
+    public static ListNode buildLinkedList(int[] nums, boolean withHead) {
+        ListNode linkList = new ListNode(-1);
+        ListNode pointer = linkList;
+        for (int num : nums) {
+            pointer.next = new ListNode(num);
+            pointer = pointer.next;
+        }
+        return withHead ? linkList : linkList.next;
+    }
+
+    public static ListNode buildLinkedList(int... nums) {
+        int[] newArray = new int[nums.length];
+        for (int i = 0; i < newArray.length; i++)
+            newArray[i] = nums[i];
+        return buildLinkedList(newArray, false);
+    }
+
+    /**
+     * 打印链表
+     * @param linkList 链表
+     */
+    public static void printLinkedList(ListNode linkList) {
+        ListNode pointer = linkList;
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        while (pointer != null) {
+            sb.append(pointer.val).append(',');
+            pointer = pointer.next;
+        }
+        sb.replace(sb.length() - 1, sb.length(), "]");
+        System.out.println(sb);
+    }
+
+    /**
+     * 打印节点信息
+     * @return 节点信息字符串
+     */
+    @Override
+    public String toString() {
+//        return "{" + val + ", " + next + '}';
+        return val + "->" + next;
+    }
+
+    public static void main(String[] args) {
+        ListNode node = ListNode.buildLinkedList(1,1,2,3,4);
+        System.out.println(node);
+    }
+}
