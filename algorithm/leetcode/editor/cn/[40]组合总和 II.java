4a6d37f178fa7e4e04539f887d94f57f6cea0b62
Axehco <mbozhong@outlook.com>
2021-12-18 09:37:19 +0800

    回溯 剪枝 模板题

--- algorithm/leetcode/editor/cn/[40]组合总和 II.java
+++ algorithm/leetcode/editor/cn/[40]组合总和 II.java
@@ -1,43 +1,43 @@
 package leetcode.editor.cn;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class combinationSumSolution {
+class combinationSum2Solution {
     List<List<Integer>> result = new ArrayList<>();
     LinkedList<Integer> path = new LinkedList<>();
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        // 横向去重 排序
+        Arrays.sort(candidates);
         backtracking(candidates, target, 0, 0);
         return result;
     }
 
     void backtracking(int[] candidates, int target, int sum, int start) {
-        // 注意这里不是剪枝，因为本题数字可以重复利用，sum > target不返回会导致栈溢出。 如果下面的for循环加了剪枝，这里就没必要写了
-        if (sum > target) return;
         if (sum == target) {
             result.add(new ArrayList<>(path));
             return;
         }
-        for (int i = start; i < candidates.length; i++) {
+
+        for (int i = start; i < candidates.length; ++i) {
+            // candidates 已经排序 这里去重 精髓在于 i > start
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
             // 剪枝
             if (sum + candidates[i] > target) continue;
             path.addLast(candidates[i]);
             sum += candidates[i];
-            // 因为数字要重复利用，因此i不做仍和处理
-            backtracking(candidates, target, sum, i);
+            backtracking(candidates, target, sum, i + 1);
             path.removeLast();
             sum -= candidates[i];
         }
     }
 
     public static void main(String[] args) {
-        combinationSumSolution solution = new combinationSumSolution();
-        int[] candidates = {2,7,6,3,5,1};
-        int target = 9;
-        System.out.println(solution.combinationSum(candidates, target));
+        combinationSum2Solution solution = new combinationSum2Solution();
+        int[] candidates = {10,1,2,7,6,1,5};
+        int target = 8;
+        System.out.println(solution.combinationSum2(candidates, target));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
