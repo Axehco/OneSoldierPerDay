9f607105c88160b3cd1b4406984bae9d0093079d
Axehco <mbozhong@outlook.com>
2021-12-27 09:27:09 +0800

    贪心

--- algorithm/leetcode/editor/cn/[134]加油站.java
+++ algorithm/leetcode/editor/cn/[134]加油站.java
@@ -2,8 +2,30 @@ package leetcode.editor.cn;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class canCompleteCircuitSolution {
-    // 暴力、超时
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i] - cost[i];
+        }
+        // 总油量小于总油耗量 肯定不能走一圈
+        if (totalGas < 0) {
+            return -1;
+        }
+        int start = 0;
+        int restGas = 0;
+        for (int i = 0; i < gas.length; i++) {
+            restGas += gas[i] - cost[i];
+            //  到达下一站的时候油量为负数 就以这个站为起点 从新计算
+            if (restGas < 0) {
+                restGas = 0;
+                start = i + 1;
+            }
+        }
+        return start;
+    }
+
+    // 暴力、超时
+    /*public int canCompleteCircuit(int[] gas, int[] cost) {
         int res = -1;
         for (int i = 0; i < gas.length; i++) {
             if (gas[i] < cost[i]) continue;
@@ -26,12 +48,14 @@ class canCompleteCircuitSolution {
             if (res != -1) break;
         }
         return res;
-    }
+    }*/
 
     public static void main(String[] args) {
         canCompleteCircuitSolution solution = new canCompleteCircuitSolution();
-        int[] gas = {1,2,3,4,5};
-        int[] cost = {3,4,5,1,2};
+//        int[] gas = {1,2,3,4,5};
+//        int[] cost = {3,4,5,1,2};
+        int[] gas = {5,1,2,3,4};
+        int[] cost = {4,4,1,5,1};
 //        int[] gas = {2,3,4};
 //        int[] cost = {3,4,3};
         System.out.println(solution.canCompleteCircuit(gas, cost));
