d023b94f6dddb6647ec7f2beec83c0fb2d83d74b
Axehco <mbozhong@outlook.com>
2022-01-03 14:06:57 +0800

    动态规划

--- algorithm/leetcode/editor/cn/[343]整数拆分.java
+++ algorithm/leetcode/editor/cn/[343]整数拆分.java
@@ -4,9 +4,10 @@ package leetcode.editor.cn;
 class integerBreakSolution {
     public int integerBreak(int n) {
         int[] dp = new int[n + 1];
+        // 0、1的初始化没有意义
         dp[2] = 1;
         for (int i = 3; i <= n; i++) {
-            for (int j = 1; j <= i / 2; j++) {
+            for (int j = 1; j <= i - 1; j++) {
                 dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
             }
         }
