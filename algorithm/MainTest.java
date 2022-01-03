a6e4b9dcf58a8b6951701734215865c8b96eb7c0
Axehco <mbozhong@outlook.com>
2022-01-03 20:11:11 +0800

    动态规划01背包测试

--- algorithm/MainTest.java
+++ algorithm/MainTest.java
@@ -8,6 +8,89 @@ import datastructure.TreeNode;
  */
 public class MainTest {
     public static void main(String[] args) {
+//        int[] weight = {1, 3, 4};
+//        int[] value = {15, 20, 30};
+//        int bagSize = 4;
+        int[] weight = {2,3,4,5};
+        int[] value = {3,4,5,8};
+        int bagSize = 8;
+//        knapsack(weight, value, bagSize);
+//        knapsack1(weight, value, bagSize);
+//        knapsack2(weight, value, bagSize);
+        knapsack33(weight, value, bagSize);
+    }
+
+    public static void knapsack2(int[] weight, int[] value, int bagSize) {
+        int[] dp = new int[bagSize + 1];
+        for (int i = 1; i < weight.length + 1; i++) {
+            for (int j = bagSize; j >= weight[i - 1]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight[i - 1]] + value[i - 1]);
+            }
+            /*for (int jj : dp) {
+                System.out.print(jj + " ");
+            }
+            System.out.println();*/
+        }
+        System.out.println("背包所获得的最大价值为：" + dp[bagSize]);
+    }
+
+    public static void knapsack33(int[] weight, int[] value, int bagSize) {
+        int[] dp = new int[bagSize + 1];
+        for (int i = 0; i < weight.length; i++) {
+            for (int j = bagSize; j >= weight[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
+            }
+            for (int jj : dp) {
+                System.out.print(jj + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("背包所获得的最大价值为：" + dp[bagSize]);
+    }
+
+    public static void knapsack(int[] weight, int[] value, int bagSize) {
+        int[][] dp = new int[weight.length + 1][bagSize + 1];
+        for (int i = 1; i < weight.length + 1; i++) {
+            for (int j = 1; j < bagSize + 1; j++) {
+                if (j < weight[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i - 1]] + value[i - 1]);
+                }
+            }
+        }
+        // 打印dp数组
+        for (int[] ints : dp) {
+            System.out.println(Arrays.toString(ints));
+        }
+        System.out.println("背包所获得的最大价值为：" + dp[weight.length][bagSize]);
+    }
+
+    public static void knapsack1(int[] weight, int[] value, int bagSize) {
+
+        int[][] dp = new int[weight.length][bagSize + 1];
+        for (int j = weight[0]; j < bagSize + 1; j++) {
+            dp[0][j] = value[0];
+        }
+
+        for (int i = 1; i < weight.length; i++) {
+            for (int j = 1; j < bagSize + 1; j++) {
+                if (j < weight[i]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+                }
+            }
+        }
+        for (int[] ints : dp) {
+            System.out.println(Arrays.toString(ints));
+        }
+        System.out.println("背包所获得的最大价值为：" + dp[weight.length - 1][bagSize]);
+    }
+
+
+
+    public static void fun11() {
         boolean[] visited = {true, true, true};
         System.out.println(visited[1]);
         fnums(visited);
