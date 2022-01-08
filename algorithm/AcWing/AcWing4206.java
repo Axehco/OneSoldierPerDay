b27f1bf79b2b1db5afbf1791ac93a4b1f631c853
Axehco <mbozhong@outlook.com>
2022-01-08 22:15:39 +0800

    acwing

---
+++ algorithm/AcWing/AcWing4206.java
@@ -0,0 +1,21 @@
+package AcWing;
+
+import java.util.Scanner;
+
+/**
+ * @author Axehco
+ * @createTime 2022年01月08日 21:45
+ */
+public class AcWing4206 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if ((str.charAt(i) - '0') == 4 || (str.charAt(i) - '0') == 7) {
+                sum++;
+            }
+        }
+        System.out.println(sum == 4 || sum == 7 ? "YES" : "NO");
+    }
+}
