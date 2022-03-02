d4c5866d77896c083e0f19e36b172b7a31838d3e
Axehco <mbozhong@outlook.com>
2022-03-02 14:50:43 +0800

    哈希表

--- algorithm/leetcode/editor/cn/[141]环形链表.java
+++ algorithm/leetcode/editor/cn/[141]环形链表.java
@@ -18,7 +18,22 @@ import java.util.Set;
  * }
  */
 class hasCycleSolution {
+    // 快慢指针
     public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 哈希表
+    /*public boolean hasCycle(ListNode head) {
         Set<ListNode> set = new HashSet<>();
         while (head != null) {
             if (set.add(head)) {
@@ -28,6 +43,6 @@ class hasCycleSolution {
             }
         }
         return false;
-    }
+    }*/
 }
 //leetcode submit region end(Prohibit modification and deletion)
