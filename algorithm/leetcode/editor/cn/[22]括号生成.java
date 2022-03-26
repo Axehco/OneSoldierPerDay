d51e0aec5a00dda8240da096439f3943f5ee59c5
Axehco <mbozhong@outlook.com>
2022-03-26 22:19:55 +0800

    进制数转化

---
+++ algorithm/leetcode/editor/cn/[22]括号生成.java
@@ -0,0 +1,29 @@
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        StringBuilder str = new StringBuilder();
+        dfs(n, res, 0, 0, str);
+        return res;
+    }
+
+    void dfs(int n, List<String> res, int leftNum, int rightNum, StringBuilder str) {
+        if (rightNum > leftNum) return;
+        if (leftNum == n && rightNum == n) {
+            res.add(str.toString());
+        }
+
+        if (leftNum < n) {
+            str.append("(");
+            dfs(n, res, leftNum + 1, rightNum, str);
+            str.delete(str.length() - 1, str.length());
+        }
+        if (rightNum < n) dfs(n, res, leftNum, rightNum + 1, str.append(")"));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
